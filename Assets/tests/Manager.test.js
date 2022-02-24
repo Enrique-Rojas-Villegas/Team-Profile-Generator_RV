5d7862a6f21bf57efba422f6858b302381b5636d
Enoc Rojas <rojiyoyo@gmail.com>
2022-02-24 15:35:11 -0600

    Changed variable name to be more understandable

--- Assets/tests/Manager.test.js
+++ Assets/tests/Manager.test.js
@@ -3,41 +3,41 @@ const Manager = require('../lib/Manager');
 describe('Manager', () => {
     it('Should receive a name only as string', () => {
         const name = 'Enoc';
-        const employee = new Manager(name);
+        const manager = new Manager(name);
 
-        expect(typeof employee.name).toBe('string');
+        expect(typeof manager.name).toBe('string');
     });
 
     it('Should recieve a valid ID as number only', () => {
-        const employee = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
-        expect(employee.id).toEqual(expect.any(Number));
+        const manager = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
+        expect(manager.id).toEqual(expect.any(Number));
     });
 
     it('Should receive a valid email containing @ symbol', () => {
         const email = 'rojiyoyo@gmail.com'
-        const employee = new Manager(email);
+        const manager = new Manager(email);
         const emailArray = email.split('');
 
         expect(emailArray).toContain('@');
     });
 
     it('Should correctly receive an officeNumber', () => {
-        const employee = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
-        expect(employee.officeNumber).toEqual(expect.any(Number));
+        const manager = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
+        expect(manager.officeNumber).toEqual(expect.any(Number));
     });
 
     it('Should correctly create a Manager object', () => {
-        const employee = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
+        const manager = new Manager('Enoc', 78, 'rojiyoyo@gmail.com', 2);
         
-        expect(typeof employee.name).toBe('string');
-        expect(employee.id).toEqual(expect.any(Number));
-        expect(employee.email).toContain('@');
-        expect(employee.officeNumber).toEqual(expect.any(Number));
+        expect(typeof manager.name).toBe('string');
+        expect(manager.id).toEqual(expect.any(Number));
+        expect(manager.email).toContain('@');
+        expect(manager.officeNumber).toEqual(expect.any(Number));
     });
 
     it('Should log the role as Manager for this class Constructor', () =>{
-        const employee = new Manager('Enoc', 78, 'rojiyoyo@gmail.com');
+        const manager = new Manager('Enoc', 78, 'rojiyoyo@gmail.com');
 
-        expect(employee.getRole()).toBe('Manager');
+        expect(manager.getRole()).toBe('Manager');
     });
 })
