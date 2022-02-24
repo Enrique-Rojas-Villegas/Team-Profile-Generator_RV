821e049a6d7b27d8432a90cbbb1bbcf1eedfcfd8
Enoc Rojas <rojiyoyo@gmail.com>
2022-02-24 15:35:59 -0600

    Changed variable name as well

--- Assets/tests/Engineer.test.js
+++ Assets/tests/Engineer.test.js
@@ -4,23 +4,23 @@ describe('Engineer', () => {
     //  1°
     it('Should receive and employee with name as string only', () => {
         const name = 'Enoc';
-        const employee = new Engineer(name);
+        const engineer = new Engineer(name);
 
-        expect(typeof employee.name).toBe('string');
+        expect(typeof engineer.name).toBe('string');
     });
 
     //  2°
     it('Should receive an ID as a number only', () => {
         //const id = 78;
-        const employee = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
-        expect(employee.id).toEqual(expect.any(Number));
+        const engineer = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
+        expect(engineer.id).toEqual(expect.any(Number));
     })
 
     //  3°
     it('Should receive an email containing the @ character to be valid email', () => {
         const email = 'rojiyoyo@gmail.com'
         const wrongEmail = 'a18110170gmail.com'
-        const employee = new Engineer(email);
+        const engineer = new Engineer(email);
         const emailArray = email.split('');
 
         expect(emailArray).toContain('@');
@@ -29,22 +29,22 @@ describe('Engineer', () => {
     //  4°
     it('Should receive a valid GitHub Username', () => {
         //const id = 78;
-        const employee = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
-        expect(typeof employee.github).toBe('string');
+        const engineer = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
+        expect(typeof engineer.github).toBe('string');
     })
 
     //  5°
     it('Should accept all parameters and create Engineer Object correctly', () => {
-        const employee = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
-        expect(typeof employee.name).toBe('string');
-        expect(employee.id).toEqual(expect.any(Number));
-        expect(employee.email).toContain('@');
+        const engineer = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
+        expect(typeof engineer.name).toBe('string');
+        expect(engineer.id).toEqual(expect.any(Number));
+        expect(engineer.email).toContain('@');
     });
 
     //  6°
     it('Should log the role as Engineer for this class Constructor', () =>{
-        const employee = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
+        const engineer = new Engineer('Enoc', 78, 'rojiyoyo@gmail.com', 'Enrique-Rojas-Villegas');
 
-        expect(employee.getRole()).toBe('Engineer');
+        expect(engineer.getRole()).toBe('Engineer');
     })
 })
